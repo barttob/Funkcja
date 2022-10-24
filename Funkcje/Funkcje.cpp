a78d9129940aff60964fea378526f23d52595b0a
asd <asd>
2022-10-24 11:37:09 +0200

    Dodanie funkcji kwadratowej

--- Funkcje/Funkcje.cpp
+++ Funkcje/Funkcje.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <cmath>
+
 
 using namespace std;
 
@@ -37,6 +39,26 @@ void f_liniowa_2() {
     }
 }
 
+void f_kwadratowa() {
+    float a, b, c, wynik, delta;
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+    cout << "Podaj c: ";
+    cin >> c;
+    delta = (b * b) - (4 * a * c);
+    if (delta > 0) {
+        cout << "X1: " << (-b - sqrt(delta)) / (2 * a) << endl;
+        cout << "X2: " << (-b + sqrt(delta)) / (2 * a);
+    }
+    else if (delta == 0)
+        cout << "X: " << (-b) / (2 * a);
+    else
+        cout << "Brak miejsc zerowych";
+    
+}
+
 int main()
 {
     int opcja;
@@ -45,6 +67,7 @@ int main()
     cout << "Wybierz opcje: " << endl;
     cout << "1. ax+b=y" << endl;
     cout << "2. Ax+By+C=0" << endl;
+    cout << "3. ax2+by+c=0" << endl;
 
     cin >> opcja;
 
@@ -56,6 +79,10 @@ int main()
 
     case 2:
         f_liniowa_2();
+        break;
+
+    case 3:
+        f_kwadratowa();
         break;
 
     default:

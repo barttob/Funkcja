5d3ccc30eeeddb32b2f1ea4fe20767d765e8426f
barttob <115357092+barttob@users.noreply.github.com>
2022-10-24 10:45:04 +0200

    Obliczanie funkcji liniowej

---
+++ Funkcje/Funkcje.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, wynik;
+
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+
+    wynik = -b / a;
+    cout << wynik;
+
+    return 0;
+}
